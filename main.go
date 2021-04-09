e29619ccccc3224f40c15887aae873dd8e808198
lbh2001 <1178055813@qq.com>
2021-04-10 00:42:25 +0800

    init gen framework

--- main.go
+++ main.go
@@ -26,13 +26,3 @@ func main() {
 	r.Run(":8080")
 
 }
-
-//func indexHandler(w http.ResponseWriter, req *http.Request){
-//	fmt.Fprintf(w,"URL.PATH=%q",req.URL.Path)
-//}
-//
-//func helloHandler(w http.ResponseWriter, req *http.Request)  {
-//	for k,v := range req.Header{
-//		fmt.Fprintf(w,"Header[%q]:%q\n",k,v)
-//	}
-//}
